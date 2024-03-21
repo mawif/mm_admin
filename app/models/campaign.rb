fc60d0923c5a4f570ed91f91e656721b9780d160
Andrew Dulson <andy@dulson.org>
2024-03-20 23:20:34 -0400

    Add validations for Campaign model

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,2 +1,4 @@
 class Campaign < ApplicationRecord
+    validates :name, presence: true
+    enum :status, { open: 0, closed: 1, archived: 2 }, prefix: true, scopes: true, default: :open
 end
--- db/migrate/20240316010240_create_campaigns.rb
+++ db/migrate/20240316010240_create_campaigns.rb
@@ -1,10 +1,10 @@
 class CreateCampaigns < ActiveRecord::Migration[7.1]
   def change
     create_table :campaigns do |t|
-      t.string :name
-      t.integer :status
-      t.boolean :accepting_reviews
-      t.boolean :accepting_interviews
+      t.string :name, null: false
+      t.integer :status, null: false, default: :open
+      t.boolean :accepting_reviews, null: false, default: false
+      t.boolean :accepting_interviews, null: false, default: false
 
       t.timestamps
     end
